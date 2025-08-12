18f9999e2a1e4d7a8f3265653226b3b246014569
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-08-12 08:50:39 -0300

    Finalizando as legendas

--- app.js
+++ app.js
@@ -2,52 +2,59 @@
 
 const imagens = [
    {
-    nome: 'Acdc',
-    url: './img/acdc.jpg'
+    nome: 'Meu Amigo Totoro na chuva com as crianças!',
+    url: './img/img1.jpg'
    },
 
    {
-    nome: 'ArcticMonkeys',
-    url: './img/arctic_monkeys.jpg'
+    nome: 'Meu Amigo Totoro e o ônibus de gato!',
+    url: './img/img2.webp'
    },
    {
-    nome: 'Bowie',
-    url:'./img/bowie.jpg'
+    nome: 'Meu Amigo Totoro deitado na grama!',
+    url:'./img/img3.webp'
    },
    {
-    nome:'HeyJude',
-    url:'./img/hey_jude.jpg'
+    nome:'O Ônibus de gato gigante!',
+    url:'./img/img4.webp'
    },
    {
-    nome: 'Kiss',
-    url:'./img/kiss.jpg'
+    nome: 'Taki e Mitsuha se comunicando por escritas no corpo!',
+    url:'./img/img5.jpg'
    },
    {
-    nome: 'Maneskin',
-    url:'./img/maneskin.jpg'
+    nome: 'Taki e seus amigos!',
+    url:'./img/img6.png'
    },
    {
-    nome: 'SilverSprings',
-    url:'./img/silver_springs.jpg'
+    nome: 'Mitsuha e Taki se encontrando!',
+    url:'./img/img7.jpg'
    }, 
    {
-    nome: 'UnderPressure', 
-    url:'./img/under_pressure.jpg'
+    nome: 'Mitsuha e seus amigos!', 
+    url:'./img/img8.jpg'
    }
 
 ]
 
 function criarImagem (srcImagem){
+
 const galeria = document.getElementById('galeria')
 const imagem = document.createElement('img')
 
+const container = document.createElement('div')
+const legenda = document.createElement('texto')
+
 imagem.src = srcImagem.url
+legenda.textContent = srcImagem.nome
+
 galeria.appendChild(imagem)
+galeria.appendChild(container)
+container.appendChild(legenda)
 
 }
 
 function carregarImagens (){
-  
 
     // criando a galeria por um while
 
